fda6eef18252b20c4650d35ff94805704ef6d3d6
Matthew Trip <matthewtrip2@gmail.com>
2020-05-05 12:07:03 +0100

    Add DI configuration method

--- NHentai.NET/Helpers/Extensions.cs
+++ NHentai.NET/Helpers/Extensions.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using NHentai.NET.Client;
 
 namespace NHentai.NET.Helpers
 {
     public static class Extensions
     {
+        public static IServiceCollection AddHentaiClient(this IServiceCollection services)
+        {
+            return services.AddScoped<IHentaiClient, HentaiClient>();
+        }
+        
         public static string ToSearchableString(this IEnumerable<string> source)
         {
             return string.Join("+", source);
