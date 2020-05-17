cbddac38b92d1dce577a6092067600751ff64234
Matthew Trip <matthewtrip2@gmail.com>
2020-05-17 22:47:14 +0100

    Create Int To DateTime Converter

---
+++ NHentai.NET/Converters/DateTimeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Serialization.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace NHentai.NET.Converters
+{
+    public class DateTimeConverter : JsonConverter<DateTime>
+    {
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var offset = DateTimeOffset.FromUnixTimeSeconds(reader.GetInt32());
+            return offset.UtcDateTime;
+        }
+
+        // Not Implemented
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
