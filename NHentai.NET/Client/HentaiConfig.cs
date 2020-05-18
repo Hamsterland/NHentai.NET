4e3635a37d0e2d450a11eb2dd2a8597747113c71
Matthew Trip <matthewtrip2@gmail.com>
2020-05-18 23:26:42 +0100

    Add New Endpoints and Move Order for Clarity

--- NHentai.NET/Client/HentaiConfig.cs
+++ NHentai.NET/Client/HentaiConfig.cs
@@ -9,7 +9,32 @@
         /// The base API url.
         /// </summary>
         public const string ApiRoot = "https://nhentai.net";
-
+        
+        /// <summary>
+        /// The front page url.
+        /// </summary>
+        public const string HomePageRoot = "/api/galleries/all?page={0}";
+        
+        /// <summary>
+        /// The base book API url.
+        /// </summary>
+        public const string BookRoot = "/api/gallery/";
+        
+        /// <summary>
+        /// The base search API url. 
+        /// </summary>
+        public const string BookSearchRoot = "/api/galleries/search?query={0}&page={1}&sort={2}";
+        
+        /// <summary>
+        /// The base related books API url.
+        /// </summary>
+        public const string RelatedSearchRoot = "/api/gallery/{0}/related";
+        
+        /// <summary>
+        /// The base tag search API url.
+        /// </summary>
+        public const string TagSearchRoot = "/api/galleries/tagged?tag_id={0}&page={1}&sort={2}";
+        
         /// <summary>
         /// The base image API url.
         /// </summary>
@@ -24,25 +49,5 @@
         /// The base image page API url.
         /// </summary>
         public const string PageSearchRoot = "/galleries/{0}/{1}.jpg";
-        
-        /// <summary>
-        /// The base book API url.
-        /// </summary>
-        public const string BookRoot = "/api/gallery/";
-        
-        /// <summary>
-        /// The base related books API url.
-        /// </summary>
-        public const string RelatedSearchRoot = "/api/gallery/{0}/related";
-        
-        /// <summary>
-        /// The base search API url. 
-        /// </summary>
-        public const string BookSearchRoot = "/api/galleries/search?query=";
-        
-        /// <summary>
-        /// The base tag search API url.
-        /// </summary>
-        public const string TagSearchRoot = "/api/galleries/tagged?tag_id=";
     }
 }
