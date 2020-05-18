8a99ac19eeafe253e1506acb662ab7429093be41
Matthew Trip <matthewtrip2@gmail.com>
2020-05-18 23:26:19 +0100

    Method Name Refactoring, Documentation Edit

--- NHentai.NET/Client/IHentaiClient.cs
+++ NHentai.NET/Client/IHentaiClient.cs
@@ -18,42 +18,53 @@ namespace NHentai.NET.Client
         /// <returns>
         /// An object with deserialized API request data.
         /// </returns>
-        Task<T> DownloadData<T>(string url);
+        Task<T> DownloadDataAsync<T>(string url);
         
         /// <summary>
-        /// Searches for a book.
+        /// Searches for a <see cref="Book"/> by its Id.
         /// </summary>
         /// <param name="id">The book Id.</param>
         /// <returns>
         /// A <see cref="Book"/> that matches the given Id. 
         /// </returns>
-        Task<Book> SearchBook(int id);
-        
+        Task<Book> SearchBookAsync(int id);
+
         /// <summary>
-        /// Searches for matched <see cref="Book"/>s.
+        /// Searches for <see cref="Book"/>s that match the given query.
         /// </summary>
+        /// <param name="page">The result page number.</param>
+        /// <param name="sort">The result sort filter.</param>
         /// <param name="query">The search query.</param>
         /// <returns>
-        /// A <see cref="SearchResult"/> that contains matched <see cref="Book"/>s.
+        /// A <see cref="SearchResult"/> that contains matching <see cref="Book"/>s.
         /// </returns>
-        Task<SearchResult> SearchQuery(params string[] query);
+        Task<SearchResult> SearchQueryAsync(int page, Sort sort, params string[] query);
         
         /// <summary>
-        /// Searches for related <see cref="Book"/>s.
+        /// Retrieves recommendations for a given <see cref="Book"/>.
         /// </summary>
         /// <param name="id">The book Id.</param>
         /// <returns>
         /// A <see cref="SearchResult"/> that contains related <see cref="Book"/>s.
         /// </returns>
-        Task<SearchResult> SearchRelated(int id);
-        
+        Task<SearchResult> SearchRelatedAsync(int id);
+
         /// <summary>
-        /// Searches for matched <see cref="Book"/>s by tag Id.
+        /// Searches for <see cref="Book"/>s that contain the given tag by its Id. 
         /// </summary>
         /// <param name="id">The tag Id.</param>
+        /// <param name="page">The result page number.</param>
+        /// <param name="sort">The result sort filter.</param>
         /// <returns>
         /// A <see cref="SearchResult"/> that contains matched <see cref="Book"/>s.
         /// </returns>
-        Task<SearchResult> SearchTag(int id);
+        Task<SearchResult> SearchTagAsync(int id, int page, Sort sort);
+
+        /// <summary>
+        /// Retrieves the current <see cref="Book"/>s on the website home page.
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns></returns>
+        Task<SearchResult> SearchHomePageAsync(int page);
     }
 }
