52b4b4ca783cac08c5ce3871e1cfc956e3cef396
Matthew Trip <matthewtrip2@gmail.com>
2020-05-18 23:25:56 +0100

    Refactor Method Names, Add Sort and Page Filter Functionality, Add Home Page Retrieval

--- NHentai.NET/Client/HentaiClient.cs
+++ NHentai.NET/Client/HentaiClient.cs
@@ -18,38 +18,45 @@ namespace NHentai.NET.Client
         private readonly HttpClient _client = new HttpClient();
 
         /// <inheritdoc />
-        public async Task<T> DownloadData<T>(string url)
+        public async Task<T> DownloadDataAsync<T>(string url)
         {
             var json = await _client.GetStringAsync(url);
             return JsonSerializer.Deserialize<T>(json);
         }
  
         /// <inheritdoc />
-        public async Task<Book> SearchBook(int id)
+        public async Task<Book> SearchBookAsync(int id)
         {
             var url = $"{HentaiConfig.ApiRoot}{HentaiConfig.BookRoot}{id}";
-            return await DownloadData<Book>(url);
+            return await DownloadDataAsync<Book>(url);
         }
         
+        
         /// <inheritdoc />
-        public async Task<SearchResult> SearchQuery(params string[] query)
+        public async Task<SearchResult> SearchQueryAsync(int page, Sort sort, params string[] query)
         {
-            var url = $"{HentaiConfig.ApiRoot}{HentaiConfig.BookSearchRoot}{query.ToSearchableString()}";
-            return await DownloadData<SearchResult>(url);
+            var url = $"{HentaiConfig.ApiRoot}{string.Format(HentaiConfig.BookSearchRoot, query.ToSearchableString(), page, sort.ToSearchableSort())}";
+            return await DownloadDataAsync<SearchResult>(url);
         }
         
         /// <inheritdoc />
-        public async Task<SearchResult> SearchRelated(int id)
+        public async Task<SearchResult> SearchRelatedAsync(int id)
         {
             var url = $"{HentaiConfig.ApiRoot}{string.Format(HentaiConfig.RelatedSearchRoot, id)}";
-            return await DownloadData<SearchResult>(url);
+            return await DownloadDataAsync<SearchResult>(url);
         }
 
         /// <inheritdoc />
-        public async Task<SearchResult> SearchTag(int id)
+        public async Task<SearchResult> SearchTagAsync(int id, int page, Sort sort)
         {
-            var url = $"{HentaiConfig.ApiRoot}{HentaiConfig.TagSearchRoot}{id}";
-            return await DownloadData<SearchResult>(url);
+            var url = $"{HentaiConfig.ApiRoot}{string.Format(HentaiConfig.TagSearchRoot, id, page, sort.ToSearchableSort())}";
+            return await DownloadDataAsync<SearchResult>(url);
+        }
+
+        public async Task<SearchResult> SearchHomePageAsync(int page)
+        {
+            var url = $"{HentaiConfig.ApiRoot}{string.Format(HentaiConfig.HomePageRoot, page)}";
+            return await DownloadDataAsync<SearchResult>(url);
         }
 
         /// <summary>
